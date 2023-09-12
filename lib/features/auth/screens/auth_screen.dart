eaf19608319f05bf72d6b4aefb347df890157f32
usamaiotexcel <usama.iotexcel@gmail.com>
2023-09-12 11:16:54 +0530

    first commit

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -164,6 +164,7 @@ class _AuthScreenState extends State<AuthScreen> {
                         const SizedBox(height: 10),
                         CustomButton(
                           text: 'Sign In',
+                          //
                           onTap: () {
                             // if (_signInFormKey.currentState!.validate()) {
                             //   signInUser();
